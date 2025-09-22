2305fe0e703ec4ee49167c02069ec2c563d5216d
むつき <kitsune0116@icloud.com>
2025-09-22 13:42:22 +0900

    0922_2コマ目

--- Object/calendar/js/Calendar.js
+++ Object/calendar/js/Calendar.js
@@ -1,97 +1,89 @@
 "use strict"
-/**
- * 1.Object フォルダを作成する
- * 2.Calender.js ファイルを作成する
- * 3.index.html ファイルを作成する
- * 
- */
 
-// console.log('hello world');
-
-/**
- * クラス宣言
- */
 class Calender {
+    element  // カレンダーを展開するエレメント
+    today    // 今日の日付を持つ
+    year     // 表示年
+    month    // 表示月 0～11まで、1月は0月とカウントする
     /**
-     * static そのクラスに所属する属性・フィールド、メソッドを宣言する
+     * 引数
+     *     id      : カレンダーを展開するエレメントのid
+     *     year    : 表示年
+     *     month   : 表示月 0～11まで、1月は0月とカウントする
+     * ※表示年と表示月はセットで指定すること
      */
-    static msg = "static field"
-    static showMsg(){
-        console.log(Calender.msg)
-    }
-    /**
-     * 属性・フィールド
-     * 先頭に「#」をつけるとプライベート宣言となる
-     */
-    #today
-    /**
-     * プロパティ
-     *  getで宣言したメソッドで値を取り出し、
-     *  setで宣言したメソッドで値を割り当てる
-     * 
-     *  get プロパティ名()
-     *  set プロパティ名(引数)
-     */
-    get today(){
-        return this.#today
-    }
-    set today(arg){
-        // arg が undefined ではない
-        // arg が Dateクラスのインスタンスである
-        if(arg != undefined && arg.constructor.name == 'Date'){
-            // 上記2つの条件を満たした場合、値をセットする
-            this.#today = arg
+    constructor(id, year, month) {
+        if (id === undefined) {
+            console.log("idが指定されていません")
+            return null
         }
-    }
-    /**
-     * コンストラクター
-     * ※ new のタイミングで動作するメソッド
-     */
-    constructor(){
-        // console.log("constructor")
-        // 確認用
+        const d = new Date()        // 時分秒までの値を持っている
+        //「今日 00:00:00」のインスタンスを生成し属性に割り当てる
+        this.today = 
+            new Date(d.getFullYear(), d.getMonth(), d.getDate())
 
-        // 属性に現在の日時を割り当てる
-        this.#today = new Date()
-        // 新しい属性を追加する
-        //      元旦の日付を割り当てる
-        this.newYerarsDay = new Date(this.#today.getFullYear(),1,1)
-    }
-    /**
-     * 現在の日時をUTC形式で記録するメソッド
-     */
-    print(){
-        let d = new Date()
-        console.log(d)
-    }
-    printToday(){
-        console.log(this.#today)
-    }
-}
+        if (year === undefined || month === undefined) {
+            if (year === undefined && month !== undefined) {
+                year = d.getFullYear()
+                month = d.getMonth()
+            }else {
+                console.log("年と月はセットで指定してください")
+                return
+            }
+        }
 
+        if((this.element = document.getElementById(id)) == null) {
+            console.log("指定されたidのエレメントが見つかりません", id)
+            return
+        }
 
-/**
- * Calenderクラスを継承したCalenderJPクラス
- * 日付を日本形式で表す
- */
-class CalenderJP extends Calender {
-    #jp
-    constructor(){
-        // 継承元のコンストラクターを呼び出す
-        super()
-        // 日付の書式を「ja-JP」とする
-        this.#jp = Intl.DateTimeFormat('ja-JP-u-ca-japanese',{
-            calender: 'japanese', // 和暦
-            era: 'long',       
-        })
+        // console.log(this.element)
+
+        // 表示年月を属性に割り当てる
+        this.year = year
+        this.month = month
+
+        console.log(id, year, month)
+
+        // コンストラクターでは初期設定のみ行う
     }
-    // メソッドのオーバーライド
-    print(){
-        // 継承元クラスのメソッドを呼び出す
-        // super.print()
-    }
-    // 本日の日付を日本形式で出力する
-    printToday(){
-        console.log(this.#jp.format(this.today))
+
+    // 指定されたエレメント内にカレンダーを構築する
+    build() {
+        // this.year年 this.month月の月初を求める
+        const startDate = new Date(this.year, this.month, 1)
+        // this.year年 this.month月の末日を求める。(翌月の0日目)
+        const endDate = new Date(this.year, this.month + 1, 0)
+
+        // constは書き換えができない定数
+        // 2個以上の月は処理しないので定数とする
+        const month = document.createElement("div")
+        month.classList.add("month")    // クラスに「month」を追加する
+        // letは何度でも書き換えが可能な変数
+        // → 複数の週を処理するためインスタンスを変更する
+        let week = document.createElement("div")
+        week.classList.add("week")      // クラスに「week」を追加する
+
+        // 月に秋を追加する
+        month.appendChild(week)
+        // 1ヶ月の繰り返し処理
+        while (startDate <= endDate) {
+            // console.log(startDate)
+            // document.createElement(エレメント名)
+            const day = document.createElement("div")
+            day.classList.add("day")    // クラスに「day」を追加する
+            day.innerText = startDate.getDate()
+            week.appendChild(day)
+            if (startDate.getDay() == 6) {
+                week = document.createElement("div")
+                week.classList.add("week")
+                month.appendChild(week)
+            }
+            // 翌日へ進む
+            startDate.setDate(startDate.getDate() + 1)
+        }
+
+        // 1ヶ月分組み上がったので追加する
+        this.element.appendChild(month)
     }
 }

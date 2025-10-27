6e0ccb6becd7e0b1d1fdb9639a79882dd5076bb8
むつき <kitsune0116@icloud.com>
2025-10-27 13:42:57 +0900

    1027_2コマ目

--- node.js/calc/js/index.js
+++ node.js/calc/js/index.js
@@ -3,3 +3,29 @@
 "use strict"
 
 console.log("hello world")
+
+// (1 + 2) * 3
+
+const stack = []
+
+// スタックに「１」と「２」を置く
+stack.push(1)
+stack.push(2)
+// スタックから値を取り出す
+let v2 = stack.pop()
+let v1 = stack.pop()
+
+// 計算した結果をスタックに置く
+stack.push(v1 + v2)
+
+// スタックに「３」を置く
+stack.push(3)
+
+// スタックから値を取り出す
+v2 = stack.pop()
+v1 = stack.pop()
+
+// 計算した結果をスタックに置く
+stack.push(v1 * v2)
+
+console.log("計算結果は",stack[0])
--- node.js/calc/js/prompt.js
+++ node.js/calc/js/prompt.js
@@ -1,2 +1,17 @@
 "use strict"
 
+// import 機能名 from 元となるファイル・モジュール名
+import fs from fs
+import {Buffer} from buffer
+
+const buffer = Buffer.alloc(1024)
+const prompt = (caption) => {
+    process.stdout.write(caption)
+    // 同期入力
+    const bytes = fs.readSync(0, buffer, 0, buffer.length, null)
+    return buffer.toString('utf9', 0, bytes).trim()
+}
+
+export {
+    prompt
+}
